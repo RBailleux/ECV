61062ebbdb8724b2ffa74717366ca593b6459fb8
robin <robin@DESKTOP-ADK8TE5>
2016-12-01 10:14:35 +0100

    randomText function

--- 2016-2017/Projet MyBlog/utilit/captcha.php
+++ 2016-2017/Projet MyBlog/utilit/captcha.php
@@ -1,3 +1,17 @@
 <?php 
 
+	/*
+	 * Generate a random sting
+	 * @param 	int		$size	Length of the string
+	 * @return 	string			A randomly generated string
+	 */
+	function generateText($size = 6){
+		$chars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','-','$','!','?'];
+		$text = '';
+		for($i = 0; $i<$size; $i++){
+			$text.=$chars[rand(0,count($chars)-1)];
+		}
+		return $text;
+	}
+
 ?>
